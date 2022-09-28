e71544c8fc8612d6596d2c1aadfd484cb2457973
hubingbing <bh38@rice.edu>
2022-09-27 20:43:36 -0500

    verify db
    Former-commit-id: d03d763661667bf399ec21fde913c6e1bdfc655b
    Former-commit-id: e9cef3979553ac4c334225f4d5516829e8234755
    Former-commit-id: 6c20772e6745d1187152d894d98124420c7fd873
    Former-commit-id: 7dd0fdbc76f53ebb350523e66d5b1eb6334de5fa [formerly b9bb0a0e3ce7780ca438d4e8a440385698c97b4a]
    Former-commit-id: 5b6fd5994f24b044fde9c5b2cf320bc8c736d0ca

--- verify_DB.py
+++ verify_DB.py
@@ -1,3 +1,22 @@
 import pandas as pd
 
 def find_repeats(path_to_db):
+    df = pd.read_csv(path_to_db, sep="\t")
+    boolean_series = df.duplicated(subset=["tax_id"])
+    repeats = df["tax_id"][boolean_series]
+    return repeats
+
+
+def verify_db(path_to_db):
+    try:
+        repeats = find_repeats(path_to_db)
+        if repeats.size > 0:
+            raise ValueError
+    except ValueError:
+        repeated_id_str = ""
+        for i in repeats:
+            repeated_id_str += str(i)+", "
+        print("There are one or more duplicate entries in this database:\n" + repeated_id_str[:-2])
+
+verify_db("/Users/hubingbing/Desktop/emu/emu_database/taxonomy.tsv")
+
