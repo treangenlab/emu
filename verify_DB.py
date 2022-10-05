19f898cedba2a6e8e03841b77fa4fb5f7594f404
hubingbing <bh38@rice.edu>
2022-10-04 23:37:50 -0500

    fixed verify_DB.py
    Former-commit-id: c53325c24a314601a4762eb735312458f297621d
    Former-commit-id: 8bd9776bbf03b15fc063cb924be94de08ee8602a
    Former-commit-id: 7d0f57399b7387f57f94e0fc4cc7231262efa669
    Former-commit-id: 8c03e2a757bec1cf70d4ae43de2c395bfeddbcd1 [formerly 77a02d83f4cfbbdc63d1ae568cf5be92e77095d7]
    Former-commit-id: 828b72e9041e27db987e94ff333f8d9ef40b426a

--- verify_DB.py
+++ verify_DB.py
@@ -1,22 +1,21 @@
+from os.path import exists
 import pandas as pd
 
-def find_repeats(path_to_db):
-    df = pd.read_csv(path_to_db, sep="\t")
+
+def find_repeats(path_to_tsv):
+    df = pd.read_csv(path_to_tsv, sep="\t")
     boolean_series = df.duplicated(subset=["tax_id"])
     repeats = df["tax_id"][boolean_series]
-    return repeats
+    return set(repeats)
 
 
 def verify_db(path_to_db):
-    try:
-        repeats = find_repeats(path_to_db)
-        if repeats.size > 0:
-            raise ValueError
-    except ValueError:
-        repeated_id_str = ""
-        for i in repeats:
-            repeated_id_str += str(i)+", "
-        print("There are one or more duplicate entries in this database:\n" + repeated_id_str[:-2])
-
-verify_db("/Users/hubingbing/Desktop/emu/emu_database/taxonomy.tsv")
+    # Check whether both species_taxid.fasta and taxonomy.tsv are present
+    if not (exists(path_to_db + "/" + "species_taxid.fasta") and exists(path_to_db + "/" + "taxonomy.tsv")):
+        raise FileNotFoundError("Missing species_taxid.fasta or taxonomy.tsv")
+    # Check for duplicates in taxonomy.tsv
+    repeats = find_repeats(path_to_db + "/" + "taxonomy.tsv")
+    if len(repeats) > 0:
+        raise ValueError("There are one or more duplicate entries in this database:\n" + str(repeats))
+    print("Database looks good!")
 
